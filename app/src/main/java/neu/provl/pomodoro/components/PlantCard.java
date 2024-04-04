1fa2c2062097bd94f39980687dc801e1297b72fb
Phạm Văn Linh <inyourarea2802@gmail.com>
2024-04-04 22:19:21 +0700

    test: Add an asset file

--- app/src/main/java/neu/provl/pomodoro/components/PlantCard.java
+++ app/src/main/java/neu/provl/pomodoro/components/PlantCard.java
@@ -5,21 +5,29 @@ import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
 import neu.provl.pomodoro.R;
+import neu.provl.pomodoro.concurrent.NetworkImageInjector;
+import neu.provl.pomodoro.data.Plant;
 
 public class PlantCard extends FrameLayout {
-    public PlantCard(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+    public PlantCard(@NonNull Context context, @NonNull Plant plant) {
+        super(context);
 
         int density = (int) context.getResources().getDisplayMetrics().density;
 
+        FrameLayout.LayoutParams LP = new LayoutParams(90 * density, 115 * density);
+        setLayoutParams(LP);
+
         FrameLayout layout = new FrameLayout(context);
-        FrameLayout.LayoutParams layoutParams = new LayoutParams(90 * density, 115 * density);
+        FrameLayout.LayoutParams layoutParams = new LayoutParams(90 * density, 100 * density);
         layoutParams.gravity = Gravity.BOTTOM;
         layout.setLayoutParams(layoutParams);
 
@@ -29,7 +37,45 @@ public class PlantCard extends FrameLayout {
 
         layout.setBackground(background);
 
+        ImageView shadow = new ImageView(context);
+        LayoutParams shadowLP = new LayoutParams(54 * density, 22 * density);
+        shadowLP.gravity = Gravity.CENTER_VERTICAL | Gravity.END;
+        shadow.setLayoutParams(shadowLP);
+        shadow.setImageResource(R.drawable.plant_shadow);
 
+        ImageView plantImg = new ImageView(context);
+        LayoutParams plantLP = new LayoutParams(65 * density, 65 * density);
+        plantImg.setLayoutParams(plantLP);
 
+        LinearLayout textInfo = new LinearLayout(context);
+        textInfo.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams textInfoLP = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        textInfoLP.gravity = Gravity.BOTTOM | Gravity.START;
+        textInfo.setLayoutParams(textInfoLP);
+
+        TextView plantName = new TextView(context);
+        plantName.setText(plant.getName());
+        plantName.setTextColor(ContextCompat.getColor(context, R.color.white));
+        plantName.setTextSize(12f);
+        plantName.setTypeface(getResources().getFont(R.font.medium));
+
+        TextView plantType = new TextView(context);
+        plantType.setText(plant.getType().name());
+        plantType.setTextColor(ContextCompat.getColor(context, R.color.white));
+        plantType.setTextSize(10f);
+        plantType.setTypeface(getResources().getFont(R.font.light));
+
+        textInfo.addView(plantName);
+        textInfo.addView(plantType);
+
+        addView(layout);
+        addView(textInfo);
+        addView(shadow);
+        addView(plantImg);
+
+        new NetworkImageInjector(plantImg).execute(plant.getImageUrl());
     }
 }
